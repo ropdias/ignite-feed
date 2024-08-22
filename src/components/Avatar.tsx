0aad21b56eb9c0cac1963b79cbab06ae024c5de7
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2024-08-21 23:32:45 -0300

    Avatar: deleting .jsx file and adding type to props

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,10 +1,17 @@
 import styles from './Avatar.module.css'
 
-export function Avatar({ hasBorder = true, src }) {
+export function Avatar({
+  hasBorder = true,
+  src,
+}: {
+  hasBorder: boolean
+  src: string
+}) {
   return (
     <img
       className={hasBorder ? styles.avatarWithBorder : styles.avatar}
       src={src}
+      alt=""
     />
   )
 }
