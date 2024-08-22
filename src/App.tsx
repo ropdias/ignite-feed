8e24010f81b26cc2937e2d77c50ae827ee10df27
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2024-08-21 23:30:10 -0300

    adding key to post.map() and formatting file

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,15 @@ const posts = [
     author: {
       avatarUrl: 'https://github.com/diego3g.png',
       name: 'Diego Fernandes',
-      role: 'CTO @Rocketseat'
+      role: 'CTO @Rocketseat',
     },
     content: [
       { type: 'paragraph', content: 'Fala galera 👋' },
-      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      {
+        type: 'paragraph',
+        content:
+          'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+      },
       { type: 'link', content: 'jane.design/doctorcare' },
     ],
     publishedAt: new Date('2022-05-03 20:00:00'),
@@ -25,16 +29,20 @@ const posts = [
     author: {
       avatarUrl: 'https://github.com/maykbrito.png',
       name: 'Mayk Brito',
-      role: 'Educator @Rocketseat'
+      role: 'Educator @Rocketseat',
     },
     content: [
       { type: 'paragraph', content: 'Fala galera 👋' },
-      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      {
+        type: 'paragraph',
+        content:
+          'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+      },
       { type: 'link', content: 'jane.design/doctorcare' },
     ],
     publishedAt: new Date('2022-05-10 20:00:00'),
   },
-];
+]
 
 export function App() {
   return (
@@ -43,9 +51,10 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          {posts.map(post => {
+          {posts.map((post) => {
             return (
               <Post
+                key={post.id}
                 author={post.author}
                 content={post.content}
                 publishedAt={post.publishedAt}
