691b643ffc5d0733c8d73a9ed15e30d7801e97cb
Rodrigo P. Dias <rodrigopdias.dev@gmail.com>
2024-09-07 11:56:36 -0300

    adding state likeCount on Comments

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import styles from './Comment.module.css'
 
 import { ThumbsUp, Trash } from 'phosphor-react'
@@ -9,9 +10,16 @@ interface CommentProps {
 }
 
 export function Comment({ content, onDeleteComment }: CommentProps) {
+  const [likeCount, setLikeCount] = useState(0)
+
   function handleDeleteComment() {
     onDeleteComment(content)
   }
+
+  function handleLikeComment() {
+    setLikeCount((previousLikeCount) => previousLikeCount + 1)
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/ropdias.png" />
@@ -35,9 +43,9 @@ export function Comment({ content, onDeleteComment }: CommentProps) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
